676636f7d390dd0b79395ef8cd659d437215e95b
Shubham Singh Rajput <rajputshubham7496@gmail.com>
2025-05-22 10:24:55 +0530

    second commit

--- src/main/java/com/grpcapi/entity/Yamaha.java
+++ src/main/java/com/grpcapi/entity/Yamaha.java
@@ -19,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Yamaha {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	private String model;
